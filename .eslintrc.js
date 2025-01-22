3b332f033f84fab80ce69d8d48a7f529e9e06f18
juanaguilar-avanza <carlosxiozi@gmail.com>
2025-01-22 10:24:28 -0600

    add update 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,14 @@ module.exports = {
   "plugins": [
     "react", "jest"
   ],
+  "settings": {
+    "react": {
+      "version": "detect"  // Detecta automáticamente la versión de React
+    }
+  },
   "rules": {
-    "indent": [
-      "error",
-        2
-      ],
+    "no-console": ["warn", { "allow": ["warn", "error"] }]  // Permite console.log solo para advertencias y errores
+  },
     "linebreak-style": [
       "error",
       "unix"
